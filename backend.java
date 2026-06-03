e935092b197ba6dcb7a358d91db9ded446b8c671
402312678 <gundomiles33@gmail.com>
2026-06-03 23:58:26 +0200

    Web Based Learning System
    creating a web based app for my for IT project
    it is used to track the student performance, analyze their ways they study, and concentration time, and creates a program around that schedule where the student can study without forcing  things

--- backend.java
+++ backend.java
@@ -1,3 +1,13 @@
-public backEnd(){
-    System.out.println("This is the backend of the project");
+
+
+public class backEnd {
+   public backEnd() {
+   }
+
+   public static void main(String[] var0) {
+      String var1 = "John Doe";
+      byte var2 = 30;
+      System.out.println("Name: " + var1);
+      System.out.println("Age: " + var2);
+   }
 }
